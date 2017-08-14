1bdd4eddd0f38c4f496130cbd366123dffc8ae16
Harish Ramachandran <harish@gitlab.com>
2017-08-14 09:23:54 -0400

    Ruby script to create a GitLab instance in docker

---
+++ gitlab_instance.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# VARIABLES
+ssh_port = 2222
+http_port = 8888
+puts "What version of GitLab do you want to use?"
+version = gets.chomp
+name = "gitlab-#{version.gsub(/-.e\.\d/, '')}"
+docker_command = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-ee:#{version}"
+
+# puts docker_command
+
+puts "\nInstalling GitLab #{version} and naming it #{name}"
+exec docker_command
+
+puts "Finished!"
