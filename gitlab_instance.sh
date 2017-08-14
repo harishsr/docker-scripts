8e2169f72205be73fd3da58ecc33e770e766bfe7
Harish Ramachandran <harish@gitlab.com>
2017-08-14 09:34:11 -0400

    Formatting

--- gitlab_instance.sh
+++ gitlab_instance.sh
@@ -8,8 +8,7 @@ version = gets.chomp
 name = "gitlab-#{version.gsub(/-.e\.\d/, '')}"
 docker_command = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-ee:#{version}"
 
-# puts docker_command
-
+# INSTALL IT
 puts "\nInstalling GitLab #{version} and naming it #{name}"
 exec docker_command
 
