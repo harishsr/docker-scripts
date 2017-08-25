54ed041f6ed2252ad26e115bd272befb5b20b9f3
Harish Ramachandran <harish@gitlab.com>
2017-08-25 10:20:32 -0400

    Ask for the type and use it

--- new_gitlab.sh
+++ new_gitlab.sh
@@ -3,10 +3,13 @@
 # VARIABLES
 ssh_port = 2222
 http_port = 8888
+puts "Do you want to install CE or EE?"
+type = gets.chomp.downcase
 puts "What version of GitLab do you want to use?"
-version = gets.chomp
+version = "#{gets.chomp}-#{type}.0"
+puts version
 name = "gitlab-#{version.gsub(/-.e\.\d/, '')}"
-docker_command = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-ee:#{version}"
+docker_command = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-#{type}:#{version}"
 
 # INSTALL IT
 puts "\nInstalling GitLab #{version} and naming it #{name}"
