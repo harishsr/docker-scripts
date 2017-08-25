d549d3aee4662b57291f4947a4e10c27e2b72f21
Harish Ramachandran <harish@gitlab.com>
2017-08-25 10:22:03 -0400

    Refactor a misnamed variable

--- new_gitlab.sh
+++ new_gitlab.sh
@@ -9,10 +9,10 @@ puts "What version of GitLab do you want to use?"
 version = "#{gets.chomp}-#{type}.0"
 puts version
 name = "gitlab-#{version.gsub(/\.\d/, '')}"
-docker_command = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-#{type}:#{version}"
+new_container = "docker run --detach --env GITLAB_OMNIBUS_CONFIG=\"external_url 'http://$(docker-machine ip gitlab-test-env):#{http_port}'; gitlab_rails['gitlab_shell_ssh_port'] = #{ssh_port};\" --hostname $(docker-machine ip gitlab-test-env) -p #{http_port}:#{http_port} -p #{ssh_port}:22 --name #{name} gitlab/gitlab-#{type}:#{version}"
 
 # INSTALL IT
 puts "\nInstalling GitLab #{version} and naming it #{name}"
-exec docker_command
+exec new_container
 
 puts "Finished!"
